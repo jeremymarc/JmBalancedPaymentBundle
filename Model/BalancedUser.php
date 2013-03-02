c7573e4f88f8bf56b5a380ea270c515575c23f5b
Jeremy Marc <jeremy.marc@me.com>
2013-03-01 17:49:57 -0800

    review BalancedUser

--- Model/BalancedUser.php
+++ Model/BalancedUser.php
@@ -4,8 +4,19 @@ namespace Jm\BalancedPaymentBundle\Model;
 
 class BalancedUser implements BalancedUserInterface
 {
-    protected $balancedUri;
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
     protected $email;
+
+    /**
+     * @var string $balancedUri
+     *
+     * @ORM\Column(name="balanced_uri", type="string", length=255)
+     */
+    protected $balancedUri;
 
     public function getEmail()
     {
@@ -26,5 +37,6 @@ class BalancedUser implements BalancedUserInterface
     public function setBalancedUri($uri)
     {
         $this->balancedUri = $uri;
+        return $this;
     }
 }
