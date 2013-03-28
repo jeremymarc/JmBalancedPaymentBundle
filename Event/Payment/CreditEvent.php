62a436b52e3cb4c18e36df7fcac264622842e2a6
Jeremy Marc <jeremy.marc@me.com>
2013-03-27 18:21:47 -0700

    update events to use BalancedPayment instead of Payment

--- Event/Payment/CreditEvent.php
+++ Event/Payment/CreditEvent.php
@@ -3,13 +3,13 @@
 namespace Jm\BalancedPaymentBundle\Event\Payment;
 
 use Symfony\Component\EventDispatcher\Event;
-use Jm\BalancedPaymentBundle\Entity\Payment;
+use Jm\BalancedPaymentBundle\Entity\BalancedPayment;
 
 class CreditEvent extends Event
 {
     private $payment;
 
-    public function __construct(Payment $payment)
+    public function __construct(BalancedPayment $payment)
     {
         $this->payment = $payment;
     }
